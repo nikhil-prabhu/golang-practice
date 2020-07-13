1de16cac370a18b4ab130c95ff7c5d93ef7023c8
Nikhil Prabhu <nikhilprabhu00@gmail.com>
2020-07-13 23:00:52 +0530

    Add stack operations.
        The `push()` method pushes data into the stack.
        The `pop()` method removes the element at the top of the stack and
        returns it.
        The `top()` method returns the element at the top of the stack.

--- Data Structures/Stack/stack.go
+++ Data Structures/Stack/stack.go
@@ -4,3 +4,23 @@ type stack struct {
 	// Stack structure
 	stackData []interface{};
 }
+
+func (s *stack) push(data ...interface{}) {
+	// Pushes data into the stack
+	for _, i := range data {
+		s.stackData = append(s.stackData, i);
+	}
+}
+
+func (s *stack) pop() interface{} {
+	// Pops and returns element at the top of the stack
+	var top interface{} = s.stackData[len(s.stackData) - 1];
+	s.stackData = s.stackData[:len(s.stackData) - 1];
+
+	return top;		// Return top of stack
+}
+
+func (s *stack) top() interface{} {
+	// Returns element at the top of the stack
+	return s.stackData[len(s.stackData) - 1];
+}
