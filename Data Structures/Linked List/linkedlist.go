a7a34ae0a876489c6918fffccd620d9344280b68
Nikhil Prabhu <nikhilprabhu00@gmail.com>
2020-07-24 23:05:04 +0530

    Add linked list operations.

--- Data Structures/Linked List/linkedlist.go
+++ Data Structures/Linked List/linkedlist.go
@@ -1,7 +1,45 @@
 package main
 
-type Node struct {
+import (
+	"fmt"
+)
+
+type node struct {
 	// Linked list node structure
 	data interface{};
-	next *Node;
+	next *node;
+}
+
+func createLinkedList() node {
+	// Create and return the head of a new linked list
+	var head node = node{data: 0, next: nil};
+
+	return head;		// Return head
+}
+
+func (n *node) insert(newData interface{}) {
+	// Insert new node at end of the list
+	var newNode node = node{data: newData, next: nil};
+
+	if n.next == nil {
+		// If list is currently empty, insert at head
+		n.next = &newNode;
+	} else {
+		var current *node;
+		
+		for current = n; current.next != nil; current = current.next {
+			// Traverse the list
+		}
+		// Insert the new node
+		current.next = &newNode;
+	}
+}
+
+func (n *node) display() {
+	// Display elements of a linked list
+	for current := n.next; current != nil; current = current.next {
+		fmt.Printf("%v -> ", current.data);
+	}
+	fmt.Printf("END");	// Signify end of the list
+	fmt.Println();
 }
